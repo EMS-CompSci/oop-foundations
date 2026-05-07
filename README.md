da54c2fdb98d3972dd327fff61c5be1c7ce0e06e
melanie-ems <43116342+melanie-ems@users.noreply.github.com>
2026-05-07 14:06:14 +0000

    fversionto be shared with Y12 2026

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # oop-foundations
+# Concepts
+
+How **inheritance** builds variants of classes by establishing 'is-a' relationships.<br>
+How **association** builds dependencies between classes by establishing 'is-a' relationships.<br>
+Purpose of **access modifier** **protected**.<br>
+How **virtual methods** allow for **polymorphism**.<br>
+
+You learn through practice with the code in this file.<br>
+
+## Key vocabulary
+
+| Term | Definition |
+|------|---------|
+| Inheritance | The feature where a **child class** is **derived** from a **parent class**, such that it encapsulates everything the parent class encapsulates with the option of encapsuate further data or methods.|
+| Association | A dependency relationship between two classes, such that one class is contained within aother. |
+| Composition | A type of association wher the containing class is fully 'owned' by the containing class. |
+| Aggregation | A type of association wher the containing class is not fully by the containing class. |
+| Protected | The access modifier which gives scope to objects of the same family. |
+| Overriding | The feature which allows for a child class to redefine a (virtual) method of the parent. |
+| Virtual Method | A method which can be overridden. |
+| Virtual Class | A class which contains a some virtual methods. |
+| Polymorphism | The implementation of different behavious of objects of related classes, achieved by overriding. |
+
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+//Class declaration
+class Point {
+
+//attributes
+public:
+    double x;
+    double y;
+
+public:
+    //constructor
+    Point(double initialX, double initialY) {
+        x = initialX; //in python self.x
+        y = initialY;
+    }
+
+    //getter methods
+    double getX() const {
+        return x;
+    }
+
+    double getY() const {
+        return y;
+    }
+
+    void setX(double newX){
+        //write the correct code here
+    }
+
+    void setY(double newY){
+        //write the correct code here
+    }
+
+    std::string toString() {
+        return "(" + std::to_string(x) + ", "
+                   + std::to_string(y) + ")";
+    }
+
+    double distanceTo(Point anotherPoint){
+        //write the correct code here
+        return 0.0;
+    }
+}; //end of class declaration 
+
+Point midPoint(Point point1, Point point2) {
+    Point midPoint(0.0,0.0);
+
+    //write the correct code here
+    return midPoint;
+}
+
+
+int main() {
+
+    Point p1(0.0, 0.0);
+    Point p2(3.0, 4.0);
+
+    double distance = 0.0;
+
+    distance = p1.distanceTo(p2);
+
+    std::cout << "p1 = " << p1.toString() << std::endl;
+    std::cout << "p2 = " << p2.toString() << std::endl;
+
+    std::cout << "p2.x = " << p2.x<< std::endl;
+    std::cout << "p2.y = " << p2.getY() << std::endl;
+    std::cout << "distance = " << distance << std::endl;
+    
+
+    return 0;
+}
